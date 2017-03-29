4782282f5b59675c335d3f2beaba4e138fe99060
hui96 <893675388@qq.com>
2017-03-29 21:31:57 +0800

    允许跨域请求

--- back-end/src/main/java/beaver/backend/HuiApplication.java
+++ back-end/src/main/java/beaver/backend/HuiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.mongo.JdkMongoSessionConverter;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableMongoHttpSession
@@ -17,5 +20,18 @@ public class HuiApplication {
 	@Bean
 	public JdkMongoSessionConverter jdkMongoSessionConverter() {
 		return new JdkMongoSessionConverter();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("*")
+						.allowedMethods("*")
+						.allowedOrigins("*")
+			};
+		};
 	}
 }
