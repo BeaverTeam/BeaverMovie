9c450c607bc61bffa91bbb8b6223a18715c53d31
shuq3 <154421897@qq.com>
2017-04-05 09:27:59 +0800

    添加热映电影的数据（暂时使用静态文件的数据）

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,9 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  myMovies : any = []
   constructor(public navCtrl: NavController) {
-
+    var request = new XMLHttpRequest();
+    request.open('GET', '../../assets/data/data.json', false);
+    request.send(null)
+    this.myMovies = JSON.parse(request.responseText);
   }
 
   gotoNotes() {
