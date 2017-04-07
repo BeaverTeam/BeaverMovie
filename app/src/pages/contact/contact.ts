b1f4f2877bb99e670e6048fb56fc2b790fa7809a
shuq3 <154421897@qq.com>
2017-04-07 19:09:59 +0800

    增加个人信息的显示

--- app/src/pages/contact/contact.ts
+++ app/src/pages/contact/contact.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { App } from 'ionic-angular';
+
 import { SettingPage }  from '../setting/setting';
+import { LoginPage } from '../login/login';
+import { Auth } from '../../providers/auth/auth.service';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public appCtrl: App, public auth: Auth) {}
 
   gotoSetting() {
     this.navCtrl.push(SettingPage);
   }
+
+  gotoLogin() {
+    this.auth.signOut();
+    this.appCtrl.getRootNav().push(LoginPage);
+  }
+
 }
