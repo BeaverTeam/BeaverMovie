b62947b34e4dec07f91ed365b89702cb9a2be2c7
Tony Zhong <tonitick@hotmail.com>
2017-04-08 21:29:17 +0800

    加入注册验证

---
+++ back-end/src/main/java/beaver/backend/controller/Validator.java
@@ -0,0 +1,20 @@
+package beaver.backend.controller;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    public static boolean isUsername(String str) {
+        String regex = "^[A-Za-z0-9]{5,10}$"; //username regex
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    public static boolean isEncryptedPassword(String str) {
+        String regex = "^[A-Za-z0-9]{16}$"; //MD5 regex
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+}
