b1928b841733286815eedd69b96c86f9215f2130
shuq3 <154421897@qq.com>
2017-05-05 19:19:40 +0800

    数据绑定

--- app/src/pages/setting/setting.ts
+++ app/src/pages/setting/setting.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { UserService } from '../../providers/user/user.service';
+import { User } from '../../providers/user/user';
+
 @Component({
   selector: 'page-setting',
   templateUrl: 'setting.html'
 })
 export class SettingPage {
+  private user: User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public userService: UserService) {
+    this.user = userService.getUser();
+  }
 
 }
