8bc0cb9d05458f540a6696a47f959b1c822c5941
shuq3 <154421897@qq.com>
2017-05-19 17:07:13 +0800

    电影详情页面

--- app/src/providers/theater/theater.service.ts
+++ app/src/providers/theater/theater.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+// import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class TheaterService {
+  // private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor() {}
 
