af0ee433f458000587aaa3072b330366df75b72d
shuq3 <154421897@qq.com>
2017-05-19 20:47:56 +0800

    将电影评分与星星数目对应

--- app/src/pages/movie-detail/movie-detail.ts
+++ app/src/pages/movie-detail/movie-detail.ts
@@ -9,9 +9,17 @@ import { TheaterService } from '../../providers/theater/theater.service';
 })
 export class MovieDetailPage {
   movie: any;
+  stars: any[] = [0, 0, 0, 0, 0];
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public theaterService: TheaterService) {
     this.movie = this.theaterService.getMovie(this.navParams.get('movieName'));
+    // 计算全星星的数目
+    this.stars = Array(Math.floor(Math.round(this.movie.rating.average) / 2)).fill(2);
+    // 计算半星星的数目
+    if (Math.round(this.movie.rating.average) % 2) this.stars.push(1);
+    // 补空星星
+    while (this.stars.length < 5) this.stars.push(0);
+    console.log(this.stars);
   }
 
 }
