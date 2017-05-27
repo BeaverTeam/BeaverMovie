a925c9196130d535977d241cd25f508e698452ba
hui96 <893675388@qq.com>
2017-05-27 23:44:51 +0800

    添加豆瓣接口

---
+++ data-service/routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+const rp = require('request-promise');
+
+const optionsForMovies = {
+    method: 'GET',
+    uri: 'http://api.douban.com/v2/movie/in_theaters',
+    json: true 
+};
+
+function getMovieOption(id) {
+  return {
+    method: 'GET',
+    uri: 'http://api.douban.com/v2/movie/subject/' + id,
+    json: true 
+  }
+}
+
+router.get('/', (req, res, next) => {
+  rp(optionsForMovies)
+    .then(body => {
+      movies = [];
+      body.subjects.forEach(function(element) {
+        let temp = "";
+        for (let genre of element.genres)
+          temp += genre + " ";
+
+        movies.push({
+          rating: element.rating.average,
+          genres: temp,
+          original_title: element.original_title,
+          title: element.title,
+          image: element.images.medium,
+          id: element.id
+        })
+      }, this);
+      res.json({state: 'success', movies: movies});
+    })
+    .catch(error => {
+      res.json({state: 'failed', err: error});
+    })
+});
+
+router.get('/movieItem/:id', (req, res, next) => {
+  rp(getMovieOption(req.params.id))
+    .then(body => res.json({state: 'success', data: body}))
+    .catch(error => res.json({state: 'failed', err: error}));
+});
+
+module.exports = router;
