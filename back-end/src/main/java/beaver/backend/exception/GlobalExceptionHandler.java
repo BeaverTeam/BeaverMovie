bd83d67219cca2cd85cc0f4cf96aa636246d4fa5
hui96 <893675388@qq.com>
2017-06-01 10:16:53 +0800

    取消Http错误状态码

--- back-end/src/main/java/beaver/backend/exception/GlobalExceptionHandler.java
+++ back-end/src/main/java/beaver/backend/exception/GlobalExceptionHandler.java
@@ -14,33 +14,29 @@ import javax.servlet.http.HttpServletRequest;
  */
 @ControllerAdvice
 @ResponseBody
+@ResponseStatus(HttpStatus.OK)
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFound.class)
     public Info userNotFoundHandler(HttpServletRequest req, UserNotFound e) {
         return new Info("failed", e.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadRequest.class)
     public Info badRequestHandler(HttpServletRequest req, BadRequest e) {
         return new Info("failed", e.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DuplicatedUserName.class)
     public Info duplicatedHandler(HttpServletRequest req, DuplicatedUserName e) {
         return new Info("failed", e.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(NotLogin.class)
     public Info notLoginHandler(HttpServletRequest req, NotLogin e) {
         return new Info("failed", e.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public Info unkownException(HttpServletRequest req) {
         return new Info("failed", "Unkown Failure");
