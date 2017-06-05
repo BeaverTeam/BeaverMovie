c65f4b421695a46f0a97066fd8cc181bd48c523f
shuq3 <154421897@qq.com>
2017-06-05 11:42:54 +0800

    着手推荐和观影券页面

--- app/src/pages/history/history.ts
+++ app/src/pages/history/history.ts
@@ -8,9 +8,21 @@ import { TheaterService } from '../../providers/theater/theater.service';
   templateUrl: 'history.html'
 })
 export class HistoryPage {
+  movies: any = [];
+  futureMovies: any = [];
+  pageNum: number = 1;
+  tickets: string = 'type1';
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public theaterService: TheaterService) {
+    theaterService.getMovies(this.pageNum).subscribe((data) => {
+      if (data.state == 'success') {
+        this.movies = data.data;
+        this.futureMovies = this.movies.splice(3, 2);
+      } else {
+        // TODO 异常处理
+      }
+    });
   }
 
 }
--- app/src/pages/recommend/recommend.ts
+++ app/src/pages/recommend/recommend.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { TheaterService } from '../../providers/theater/theater.service';
+
 @Component({
   selector: 'page-recommend',
   templateUrl: 'recommend.html'
 })
 export class RecommendPage {
+  movies: any = [];
+  pageNum: number = 1;  // 当前获取的电影页数
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public theaterService: TheaterService) {
+    theaterService.getMovies(this.pageNum).subscribe((data) => {
+      if (data.state == 'success') {
+        this.movies = data.data;
+      } else {
+        // TODO 异常处理
+      }
+    });
+  }
 
 }
