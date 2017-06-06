5eefef71d767e43a3bb87a7f46b7836dc214d084
hui96 <893675388@qq.com>
2017-06-06 12:37:00 +0800

    增加gitignore，用户登录失败使用原代码

--- data-service/app.js
+++ data-service/app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+  res.header("Access-Control-Expose-Headers", "Set-Cookie");
+  res.header("Access-Control-Allow-Headers", "Origin, Authorization, Content-Type, Accept, Set-Cookie");
+  next();
+});
+
 app.use('/', routes);
 app.use('/save-file', filesaver);
 
